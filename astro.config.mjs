536f4de6a1500e84986795a0f6253824aca58fea
Bastien <record.bastien@gmail.com>
2024-12-28 16:44:39 +0100

    feat: add language button

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    i18n: {
+        defaultLocale: 'fr',
+        locales: ['fr', 'en'],
+        routing: {
+            prefixDefaultLocale: false,
+        }
+    },
 });
